ba859ca8a6a3255f5718077aff129012b97da27a
dmytrashvasyl <dmytrashvasyl@gmail.com>
2024-11-25 11:39:06 +0200

    add main program.cs and dop static slass

---
+++ HomeTask12.1/FibonacciCalculator.cs
@@ -0,0 +1,16 @@
+﻿namespace HomeTask12._1;
+
+internal static class FibonacciCalculator
+{
+    internal static int GetFibonacci(int n)
+    {
+        if (n == 1 || n == 2)
+        {
+            return 1;
+        }
+        else
+        {
+            return GetFibonacci(n - 1) + GetFibonacci(n - 2);
+        }
+    }
+}
---
+++ HomeTask12.1/Program.cs
@@ -0,0 +1,15 @@
+﻿using HomeTask12._1;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.Write("Введіть номер числа Фібоначчі: ");
+
+if (int.TryParse(Console.ReadLine(), out int n) && n > 0)
+{
+    int result = FibonacciCalculator.GetFibonacci(n);
+    Console.WriteLine($"Число Фібонначі для n={n}: {result}");
+}
+else
+{
+    Console.WriteLine("Будь ласка, введіть дійсне число");
+}
