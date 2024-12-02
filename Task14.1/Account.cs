848a5e9fc3759fbf40a387061411d307ab50cbb9
dmytrashvasyl <dmytrashvasyl@gmail.com>
2024-12-02 15:03:03 +0200

    add Task14.1

---
+++ Task14.1/Account.cs
@@ -0,0 +1,49 @@
+﻿namespace HomeTask14._1;
+
+public class Account
+{
+    private string name;
+    private double balance;
+
+    public Account(string name, double initialBalance)
+    {
+        this.name = name;
+        this.balance = initialBalance >= 0 ? initialBalance : 0;
+    }
+
+    public void Deposit(double amount)
+    {
+        if (amount > 0)
+        {
+            balance += amount;
+        }
+        else
+        {
+            Console.WriteLine("You cannot deposit more than ${0}", amount);
+        }
+    }
+
+    public void Withdraw(double amount)
+    {
+        if (amount > 0)
+        {
+            if (balance >= amount)
+            {
+                balance -= amount;
+            }
+            else
+            {
+                Console.WriteLine("You cannot withdraw more than ${0}", amount);
+            }
+        }
+        else
+        {
+            Console.WriteLine("You cannot withdraw less than ${0}", amount);
+        }
+    }
+
+    public double Balance
+    {
+        get { return balance; }
+    }
+}
---
+++ Task14.1/Program.cs
@@ -0,0 +1,9 @@
+﻿using HomeTask14._1;
+
+Account heikkisAccount = new Account("Heikki's account", 100.00);
+Account heikkisSwissAccount = new Account("Heikki's account in Switzerland", 1000000.00);
+
+heikkisAccount.Withdraw(20);
+Console.WriteLine("The balance of Heikki's account is now: " + heikkisAccount.Balance);
+heikkisSwissAccount.Deposit(200);
+Console.WriteLine("The balance of Heikki's other account is now: " + heikkisSwissAccount.Balance);
