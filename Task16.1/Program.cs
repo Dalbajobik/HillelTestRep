575e0195c29e537a4a9490ddde04c22a4ff1d80a
dmytrashvasyl <dmytrashvasyl@gmail.com>
2024-12-06 16:49:10 +0200

    add Task16.1

---
+++ Task16.1/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+ 
+Console.Write("Введіть шлях до вихідного файлу: ");
+string? sourceFilePath = Console.ReadLine();
+
+Console.Write("Введіть шлях до файлу, в який потрібно скопіювати дані: ");
+string? destinationFilePath = Console.ReadLine();
+
+try
+{
+    File.Copy(sourceFilePath, destinationFilePath, true);
+    Console.WriteLine("Файл успішно скопійовано!");
+}
+catch (FileNotFoundException)
+{
+    Console.WriteLine("Вихідний файл не знайдено. Перевірте шлях.");
+}
+catch (IOException ex)
+{
+    Console.WriteLine($"Сталася помилка при копіюванні файлу: {ex.Message}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Сталася помилка: {ex.Message}");
+}
